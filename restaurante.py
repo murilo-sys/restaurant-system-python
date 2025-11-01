3afcac6faf9c5a3f42f7be7f4ae415f74bde93e8
Murilo <murilod_santiago@hotmail.com>
2025-11-01 11:32:40 -0300

    refactor: string acompanha True e False na classe.

--- restaurante.py
+++ restaurante.py
@@ -3,31 +3,34 @@ import time
 
 from services import restaurante_service
 
-nome = input("Digite o nome do restaurante \n")
-os.system("cls")
-
-restaurante = restaurante_service.Restaurante(nome)
-
-while True:
+def limpar_tela():
     os.system("cls")
 
+nome = input("Digite o nome do restaurante \n")
+restaurante = restaurante_service.Restaurante(nome)
+limpar_tela()
+
+
+while True:
+    limpar_tela()
+
     print(f" {nome.title()} ".center(30, "="))
+
     opcao = int(input("Escolha uma opção:\n" \
     "[1] Registrar venda\n"
     "[2] Visualizar resultados\n"
     "[9] Sair\n"
     "Sua opção: "))
     
-    os.system("cls")
+    limpar_tela()
     match opcao:
         case 1:
             preco_produto = float(input("Insira o valor do produto\nR$"))
-            os.system("cls")
+            limpar_tela()
 
-            if restaurante.registrar_venda(preco_produto):
-                print(f"Prato registrado no valor de R${preco_produto}")
-            else:
-                 print("Valor inválido, favor inserir um valor maior que 0")
+            sucess, msg = restaurante.registrar_venda(preco_produto)
+            print(msg)
+            
             time.sleep(2)
 
         case 2:
--- services/restaurante_service.py
+++ services/restaurante_service.py
@@ -5,13 +5,12 @@ class Restaurante:
         self.rendimento = 0.0
         self.clientes = 0
 
-    def registrar_venda(self, preco_produto: float) -> bool:
+    def registrar_venda(self, preco_produto: float) -> tuple[bool, str]:
         if preco_produto > 0:
             self.rendimento += preco_produto
             self.clientes += 1
-            return True
-        else:
-            return False
+            return True, f"Prato registrado no valor de R${preco_produto}"
+        return False, "Valor inválido, favor inserir um valor maior que 0"
 
     def resultados (self):
         print(f" Resultados ".center(25,"="), 
